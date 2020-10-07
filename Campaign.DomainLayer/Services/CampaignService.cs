215373704d95442a01f4f09623cac84ea5e6b110
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-10-07 23:13:06 +0300

    Servis katmanının oluşturulması

---
+++ Campaign.DomainLayer/Services/CampaignService.cs
@@ -0,0 +1,12 @@
+﻿using Campaign.DomainLayer.IServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campaign.DomainLayer.Services
+{
+    public class CampaignService:ICampaignService
+    {
+
+    }
+}
---
+++ Campaign.DomainLayer/Services/OrderService.cs
@@ -0,0 +1,11 @@
+﻿using Campaign.DomainLayer.IServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campaign.DomainLayer.Services
+{
+    public class OrderService:IOrderService
+    {
+    }
+}
