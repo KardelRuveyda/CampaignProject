6cd48e0526154d7f3191f3709383163f8895dfb7
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-10-09 01:04:18 +0300

    Presentation Layer

---
+++ Campaign.PresentationLayer/DependencyLoader.cs
@@ -0,0 +1,25 @@
+﻿using Campaign.AppLayer;
+using Campaign.DomainLayer.IServices;
+using Campaign.DomainLayer.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Campaign.PresentationLayer
+{
+    public class DependencyLoader
+    {
+        public ServiceProvider Init()
+        {
+            ServiceCollection serviceDescriptors = new ServiceCollection();
+            serviceDescriptors.AddSingleton<ICampaignService, CampaignService>();
+            serviceDescriptors.AddSingleton<IOrderService, OrderService>();
+            serviceDescriptors.AddSingleton<IProductService, ProductService>();
+            serviceDescriptors.AddSingleton<ICommandManager, CommandManager>();
+            return serviceDescriptors.BuildServiceProvider();
+
+        }
+    }
+}
---
+++ Campaign.PresentationLayer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Campaign.AppLayer;
+using System;
+using System.Windows.Input;
+
+namespace Campaign.PresentationLayer
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            DependencyLoader dependencyLoader = new DependencyLoader();
+            var provider = dependencyLoader.Init();
+
+            var commandParser = (ICommandManager)provider.GetService(typeof(ICommandManager));
+
+            var keys = Read();
+
+            commandParser.Execute(keys.command, keys.objs);
+
+
+        }
+
+        private static (string command, string[] objs) Read()
+        {
+            var input = Console.ReadLine().Split(' ');
+
+            return (input[0], input[1..]);
+
+        }
+    }
+}
