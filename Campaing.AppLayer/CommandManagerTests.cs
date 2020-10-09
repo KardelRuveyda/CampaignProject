d1f368da379f4ea5b25aa94b3168d890a14d3e28
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-10-09 19:36:21 +0300

    xUnit test projesi Eklendi AppLayer için

---
+++ Campaing.AppLayer/CommandManagerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Campaing.AppLayer
+{
+    public class CommandManagerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
